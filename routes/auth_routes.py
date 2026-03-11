ae8ecd4a9aa962503c27fa0a25e8c0bc482ffa43
duality314 <pie_piepy@163.com>
2026-03-11 22:16:35 +0800

    feat: migrate to APIFlask, enhance error handling, and update route schemas

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,30 +1,33 @@
-from flask import Blueprint, request, jsonify
+from apiflask import APIBlueprint
 from flask_jwt_extended import create_access_token
 
 from controllers.auth_controller import register_user, authenticate_user
+from schemas import AuthLoginInSchema, AuthLoginOutSchema, AuthRegisterInSchema, StatusSchema
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = APIBlueprint("auth", __name__)
 
-@auth_bp.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()  # 获取请求JSON数据
-    username = data.get('username') if data else None
-    password = data.get('password') if data else None
+@auth_bp.post("/register")
+@auth_bp.input(AuthRegisterInSchema , arg_name = "data")
+@auth_bp.output(StatusSchema, 200)
+def register(data):
+    username = data["username"]
+    password = data["password"]
     # 调用控制器执行注册逻辑
     user_info = register_user(username, password)
-    return jsonify(user_info), 200
+    return user_info
 
-@auth_bp.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    username = data.get('username') if data else None
-    password = data.get('password') if data else None
+@auth_bp.post("/login")
+@auth_bp.input(AuthLoginInSchema)
+@auth_bp.output(AuthLoginOutSchema, 200)
+def login(data):
+    username = data["username"]
+    password = data["password"]
     # 验证用户名密码
     user = authenticate_user(username, password)
     # 生成JWT令牌，有效期7天，载荷包含用户id（作为身份）和用户名
     access_token = create_access_token(identity=str(user.id), additional_claims={"username": user.username}, expires_delta=False)
     # 返回令牌和用户信息
-    return jsonify({
+    return {
         "token": access_token,
         "user": { "id": user.id, "username": user.username }
-    }), 200
+    }
